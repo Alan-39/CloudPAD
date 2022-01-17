3da5975707fbca1dfc2fabf59c926df1c035c14d
Alanfy3 <alanthekurayami@gmail.com>
2022-01-17 19:11:50 +0800

    Added Error message component

---
+++ client/src/components/ErrorMsg.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-alert
+        type="error"
+        v-bind:max-width="maxWidth"
+        dismissible
+    >
+    {{ message }}
+    </v-alert>
+</template>
+
+<script>
+    export default {
+        props: ['message', 'maxWidth']
+    }
+</script>
