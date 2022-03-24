1dd4799801651123232159a2658b4672c270a683
Alanfy3 <alanthekurayami@gmail.com>
2022-03-24 10:01:04 +0800

    Added Generate and Get invite code API

--- server/api/UserReferral.js
+++ server/api/UserReferral.js
@@ -1,11 +1,44 @@
 const router = require('express').Router();
+const { nanoid } = require('nanoid');
+const InviteCode = require('../models/InviteCode');
+
+router.get('/getcode', (req, res) => {
+    if (req.user.role === "admin") {
+        InviteCode.find({})
+            .then(data => {
+                res.send(data);
+            }).catch(err => {
+                console.log(err);
+            });
+        return;
+    }
+    res.send({ message: "foo, i'm afraid you cannot do that" })
+});
 
 router.post('/gencode', (req, res) => {
-    // checks if user is authorized to perform action
-    // generate invite code
-    // save code to mongodb
-    // return invite code result
-    res.send({ message: "foo"})
+    if (req.user.role === "admin") {
+        const inviteCode = nanoid(10);
+        const inviteDetails = {
+            _id: inviteCode,
+            generatedBy: req.user.username,
+            usedBy: '',
+        }
+        InviteCode.create(inviteDetails);
+        return res.send(inviteDetails);
+    }
+    res.send({ message: "foo, i'm afraid you cannot do that" })
+});
+
+router.post('/delcode', (req, res) => {
+    if (req.user.role === "admin") {
+        InviteCode.deleteOne({ _id: req.body._id })
+            .then(res => {
+                console.log(res);
+            }).catch(err => {
+                console.log(err);
+            });
+        return;
+    }
 });
 
 module.exports = router
