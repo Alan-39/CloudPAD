4156c6145bf2a111d3a0dcdb609961d81a07efde
alan <alanthekurayami@gmail.com>
2022-09-12 17:26:08 +0800

    Added login page for vuejs

--- CloudPAD-UI/src/views/LoginView.vue
+++ CloudPAD-UI/src/views/LoginView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="p-4 w-full max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8">
+  <div class="p-4 w-full max-w-sm bg-secondary rounded-lg border border-gray-200 shadow-md sm:p-6 md:p-8">
     <Form @submit="login" :validation-schema="schema" v-slot="{ errors }" class="space-y-6">
-      <h5 class="text-xl font-medium text-gray-900">CloudPAD</h5>
+      <h5 class="text-xl font-medium text-gray-700">CloudPAD</h5>
       <div>
-        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Username</label>
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-700 ">Username</label>
         <Field id="username" name="username" type="text"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          class="bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           placeholder="bruh6699" required />
         <span class="text-sm text-red-600">{{ errors.username }}</span>
       </div>
       <div>
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-700">Password</label>
         <Field id="password" name="password" type="password" placeholder="•••••••••"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          class="bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
           required />
         <span class="text-sm text-red-600">{{ errors.password }}</span>
       </div>
@@ -24,13 +24,20 @@
             <input id="remember" type="checkbox" value=""
               class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300">
           </div>
-          <label for="remember" class="ml-2 text-sm font-medium text-gray-900">Remember me</label>
+          <label for="remember" class="ml-2 text-sm font-medium text-gray-700">Remember me</label>
         </div>
       </div>
       -->
       <button
-        class="w-full text-white bg-sky-600 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ease-in-out duration-300">
-        Login
+        class="w-full flex justify-center text-white hover:text-gray-700 bg-accent-dark hover:bg-accent focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 text-center ease-in-out duration-300">
+        <svg v-if="isLoading" class="animate-spin h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+          </path>
+        </svg>
+        <span v-if="!isLoading">Login</span>
       </button>
       <span v-if="errMsg !== ''" class="my-4 text-sm text-center text-red-600">{{ errMsg }}</span>
     </Form>
@@ -45,6 +52,7 @@ import * as yup from 'yup';
 import { useAuthStore } from '../stores/auth.store';
 
 const errMsg = ref('');
+const isLoading = ref(false);
 
 const schema = yup.object({
   username: yup.string().required().min(6),
@@ -55,16 +63,19 @@ useForm({
   validationSchema: schema,
 });
 
-function login(values) {
+async function login(values) {
+  isLoading.value = true;
   const authStore = useAuthStore();
 
-  authService.login(values).then(response => {
+  try {
+    const response = await authService.login(values);
     errMsg.value = '';
     authStore.setSession(response.data.token)
-    console.log(authStore.token);
-  }).catch(err => {
+  } catch(err) {
     errMsg.value = err.response.data.message
-  });
+  } finally {
+    isLoading.value = false;
+  }
 }
 </script>
 
