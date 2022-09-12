77e0cb36a72738c881fc33ec62669fd2ad95c044
alan <alanthekurayami@gmail.com>
2022-09-12 17:27:43 +0800

    Added media card component

---
+++ CloudPAD-UI/src/components/MediaCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-white hover:bg-gray-300 ease-in-out duration-300 rounded-2xl shadow-md">
+    <div class="p-4">
+      <div class="flex justify-center text-gray-500">
+
+        <svg v-if="iconType === 'folder'" xmlns="http://www.w3.org/2000/svg" width="100" fill="none" viewBox="0 0 24 24"
+          strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+            d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+        </svg>
+        <svg v-if="iconType === 'file'" xmlns="http://www.w3.org/2000/svg" width="100" fill="none" viewBox="0 0 24 24"
+          strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+            d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+        </svg>
+        <svg v-if="iconType === 'server'" xmlns="http://www.w3.org/2000/svg" width="100" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+          className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+            d="M21.75 17.25v-.228a4.5 4.5 0 00-.12-1.03l-2.268-9.64a3.375 3.375 0 00-3.285-2.602H7.923a3.375 3.375 0 00-3.285 2.602l-2.268 9.64a4.5 4.5 0 00-.12 1.03v.228m19.5 0a3 3 0 01-3 3H5.25a3 3 0 01-3-3m19.5 0a3 3 0 00-3-3H5.25a3 3 0 00-3 3m16.5 0h.008v.008h-.008v-.008zm-3 0h.008v.008h-.008v-.008z" />
+        </svg>
+
+
+      </div>
+      <h1 class="text-2xl text-center overflow-hidden truncate font-medium text-slate-600 pb-2">{{ name }}</h1>
+    </div>
+  </div>
+</template>
+  
+<script setup>
+defineProps(['name', 'iconType']);
+</script>
+  
