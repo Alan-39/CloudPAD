86a3f115b00c0bef691727f3f712c0e178ef78e1
alan <alanthekurayami@gmail.com>
2022-09-12 17:25:45 +0800

    Added custom colour theme

--- CloudPAD-UI/tailwind.config.js
+++ CloudPAD-UI/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: { // use 60/30/10 rule
+        'primary': '#F1F9F7',
+        'primary-dark': '#49AB93',
+        'secondary': '#ACDDE7',
+        'secondary-dark': '#2D8A9C',
+        'accent': '#ADB9E3',
+        'accent-dark': "#334894",
+  
+      },
+    },
   },
   plugins: [],
 }
