14c29b5327d88ae6406e79635cb6cbcb0fefc02d
alan <alanthekurayami@gmail.com>
2023-02-17 21:34:35 +0800

    edited api cors

--- CloudPAD-API/src/main.ts
+++ CloudPAD-API/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://127.0.0.1:5173', 'http://localhost:5173'] })
+  app.enableCors({ origin: ['http://cloudpad-vuejs.s3-website-ap-southeast-1.amazonaws.com'] })
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
